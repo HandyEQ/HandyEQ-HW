41ac08eeb8707bfccc1fb190ab49bc79c9e605de
Einar Nilsson <einarn@19.ed4225.lab.cse.a.1.hb.cdal.chalmers.se>
2014-05-12 15:06:20 +0200

    Added the a protocol for reading

--- Firmware/main.c
+++ Firmware/main.c
@@ -87,6 +87,378 @@ void new_uart(){
 }
 
 void uart_input(){
-	char i = recieve_uart();
-	printf("%c", i);
+	//Pointer to base of buffer array
+	char * input = uartBuffers->buffer[(uartBuffer->bufferSelect-1)%2];
+
+	//Size of the buffer		
+	int size = uartBuffers->counter[(uartBuffer->bufferSelect-1)%2];
+
+	//The tempStr is used to temporary store the values read before converting them into an int.
+	char tempStr[7];
+
+	//This value is used to store the converted values before storing them in their correct variable.
+	int tempVal = 0;
+
+	for(i = 0; i < size; i+11){
+		if(input[i] == 'S'){
+			//If an effect in a box is to be changed.
+			if((input[i+1] == '1')){
+				//If the box effect that is changed is in the first box.
+				if((input[i+3] == '0')){
+					//If the new effect is bypass.
+				}else if((input[i+3] == '1')){
+					//If the new effect is noeffect.
+				}else if((input[i+3] == '2')){
+					//If the new effect is equalizer.
+				}else if((input[i+3] == '3')){
+					//If the new effect is volume.
+				}else if((input[i+3] == '4')){
+					//If the new effect is delay.
+				}			
+			}else if(input[i+1] == '2'){
+				//If the box effect that is changed is in the second box.
+				if((input[i+3] == '0')){
+					//If the new effect is bypass.
+				}else if((input[i+3] == '1')){
+					//If the new effect is noeffect.
+				}else if((input[i+3] == '2')){
+					//If the new effect is equalizer.
+				}else if((input[i+3] == '3')){
+					//If the new effect is volume.
+				}else if((input[i+3] == '4')){
+					//If the new effect is delay.
+				}
+			}else if(input[i+1] == '3'){
+				//If the box effect that is changed is in the third box.
+				if((input[i+3] == '0')){
+					//If the new effect is bypass.
+				}else if((input[i+3] == '1')){
+					//If the new effect is noeffect.
+				}else if((input[i+3] == '2')){
+					//If the new effect is equalizer.
+				}else if((input[i+3] == '3')){
+					//If the new effect is volume.
+				}else if((input[i+3] == '4')){
+					//If the new effect is delay.
+				}
+			}else if(input[i+1] == '4'){
+				//If the box effect that is changed is in the forth box.
+				if((input[i+3] == '0')){
+					//If the new effect is bypass.
+				}else if((input[i+3] == '1')){
+					//If the new effect is noeffect.
+				}else if((input[i+3] == '2')){
+					//If the new effect is equalizer.
+				}else if((input[i+3] == '3')){
+					//If the new effect is volume.
+				}else if((input[i+3] == '4')){
+					//If the new effect is delay.
+				}
+			}
+		}else if(input[i] == '1'){
+			//If a setting for the first effect box has been changed.
+			if((input[i+1] == 'E')){
+				//The new value is for the equalizer effect.
+				//These value vary from 0 - 8 where 0 represent -12 dB.
+				if((input[i+2] == 'B')){
+					//The value is for the bass.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the bass value in the first effect box.
+					tempVal = atoi(tempStr);
+				}else if((input[i+2] == 'M')){
+					//The value is for the mid.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the mid value in the first effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'T')){
+					//The value is for the treble.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the treble value in the first effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else if((input[i+1] == 'V')){
+				//The new value is for the volume effect.
+				//This value vary from 0 -(-99)dB.
+				//There is only one value for the volume.
+				//The value that is to be used starts at input[i+4]
+				//This value is stored in the tempStr to be converted without getting unwanted values.
+				strncpy(tempStr, input[i+4], 6);
+				tempStr[6] = '\0';
+				//The tempVal should contain the value that is to be stored as the volume value in the first effect box.
+				tempVal = atoi(tempStr);
+			}else if((input[i+1] == 'D')){
+				//The new value is for the delay effect.
+				//The value that is to be used starts at input[i+4]
+				if((input[i+2] == 'T')){
+					//The value is for the delay time.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the delay time value in the first effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'G')){
+					//The value is for the gain.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of gain value
+					//in the first effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'F')){
+					//The value is for the feedback.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of feedback value
+					//in the first effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else{
+				//The value is for the bypass or no effect which have no values.
+			}		
+		}else if(input[i] == '2'){
+			//If a setting for the second effect box has been changed.
+			if((input[i+1] == 'E')){
+				//The new value is for the equalizer effect.
+				//These value vary from 0 - 8 where 0 represent -12 dB.
+				if((input[i+2] == 'B')){
+					//The value is for the bass.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the bass value in the second effect box.
+					tempVal = atoi(tempStr);
+				}else if((input[i+2] == 'M')){
+					//The value is for the mid.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the mid value in the second effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'T')){
+					//The value is for the treble.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the treble value in the second effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else if((input[i+1] == 'V')){
+				//The new value is for the volume effect.
+				//This value vary from 0 -(-99)dB.
+				//There is only one value for the volume.
+				//The value that is to be used starts at input[i+4]
+				//This value is stored in the tempStr to be converted without getting unwanted values.
+				strncpy(tempStr, input[i+4], 6);
+				tempStr[6] = '\0';
+				//The tempVal should contain the value that is to be stored as the volume value in the second effect box.
+				tempVal = atoi(tempStr);
+			}else if((input[i+1] == 'D')){
+				//The new value is for the delay effect.
+				//The value that is to be used starts at input[i+4]
+				if((input[i+2] == 'T')){
+					//The value is for the delay time.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the delay time value in the second effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'G')){
+					//The value is for the gain.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of gain value
+					//in the second effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'F')){
+					//The value is for the feedback.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of feedback value
+					//in the second effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else{
+				//The value is for the bypass or no effect which have no values.
+			}
+		}else if(input[i] == '3'){
+			//If a setting for the third effect box has been changed.
+			if((input[i+1] == 'E')){
+				//The new value is for the equalizer effect.
+				//These value vary from 0 - 8 where 0 represent -12 dB.
+				if((input[i+2] == 'B')){
+					//The value is for the bass.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the bass value in the third effect box.
+					tempVal = atoi(tempStr);
+				}else if((input[i+2] == 'M')){
+					//The value is for the mid.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the mid value in the third effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'T')){
+					//The value is for the treble.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the treble value in the third effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else if((input[i+1] == 'V')){
+				//The new value is for the volume effect.
+				//This value vary from 0 -(-99)dB.
+				//There is only one value for the volume.
+				//The value that is to be used starts at input[i+4]
+				//This value is stored in the tempStr to be converted without getting unwanted values.
+				strncpy(tempStr, input[i+4], 6);
+				tempStr[6] = '\0';
+				//The tempVal should contain the value that is to be stored as the volume value in the third effect box.
+				tempVal = atoi(tempStr);
+			}else if((input[i+1] == 'D')){
+				//The new value is for the delay effect.
+				//The value that is to be used starts at input[i+4]
+				if((input[i+2] == 'T')){
+					//The value is for the delay time.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the delay time value in the third effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'G')){
+					//The value is for the gain.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of gain value
+					//in the third effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'F')){
+					//The value is for the feedback.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of feedback value
+					//in the third effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else{
+				//The value is for the bypass or no effect which have no values.
+			}
+		}else if(input[i] == '4'){
+			//If a setting for the forth effect box has been changed.
+			if((input[i+1] == 'E')){
+				//The new value is for the equalizer effect.
+				//These value vary from 0 - 8 where 0 represent -12 dB.
+				if((input[i+2] == 'B')){
+					//The value is for the bass.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the bass value in the forth effect box.
+					tempVal = atoi(tempStr);
+				}else if((input[i+2] == 'M')){
+					//The value is for the mid.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the mid value in the forth effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'T')){
+					//The value is for the treble.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the treble value in the forth effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else if((input[i+1] == 'V')){
+				//The new value is for the volume effect.
+				//This value vary from 0 -(-99)dB.
+				//There is only one value for the volume.
+				//The value that is to be used starts at input[i+4]
+				//This value is stored in the tempStr to be converted without getting unwanted values.
+				strncpy(tempStr, input[i+4], 6);
+				tempStr[6] = '\0';
+				//The tempVal should contain the value that is to be stored as the volume value in the forth effect box.
+				tempVal = atoi(tempStr);
+			}else if((input[i+1] == 'D')){
+				//The new value is for the delay effect.
+				//The value that is to be used starts at input[i+4]
+				if((input[i+2] == 'T')){
+					//The value is for the delay time.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the delay time value in the forth effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'G')){
+					//The value is for the gain.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of gain value
+					//in the forth effect box.
+					tempVal = atoi(tempStr);	
+				}else if((input[i+2] == 'F')){
+					//The value is for the feedback.
+					//This value is in float point.
+					//The value that is to be used starts at input[i+4]
+					//This value is stored in the tempStr to be converted without getting unwanted values.
+					strncpy(tempStr, input[i+4], 6);
+					tempStr[6] = '\0';
+					//The tempVal should contain the value that is to be stored as the float point of feedback value
+					//in the forth effect box.
+					tempVal = atoi(tempStr);	
+				} 
+			}else{
+				//The value is for the bypass or no effect which have no values.
+			}
+		}else if(input[i] == 'I'){
+			//Used for when the GUI is connected and need all the current values from the board.
+		}
+	}
+	//char i = recieve_uart();
+	//printf("%c", i);
 }
